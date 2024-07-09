d98227730a90d60803e0e36cd4340605ec7473ee
Eric Williams <ewilliams0305@gmail.com>
2024-07-09 18:12:34 -0400

    added path combine

--- tests/SimplSharp.Tool.Tests/Setup/ClzContainerFixture.cs
+++ tests/SimplSharp.Tool.Tests/Setup/ClzContainerFixture.cs
@@ -16,17 +16,17 @@ public class ClzContainerFixture : IAsyncLifetime
     public string ReferencedSdkVersion { get; set; } = string.Empty;
 
 #if DEBUG
-    public const string ExePath = @"..\..\..\..\..\source\SimplSharp.Tool\bin\Debug\net8.0\SimplSharp.Tool.exe";
-    public const string TargetLibraryPath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Debug\net472\SimplSharp.Library.dll";
-    public const string TargetLibraryProject = @"..\..\..\..\..\source\SimplSharp.Library\SimplSharp.Library.csproj";
-    public const string TargetArchivePath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Debug\net472\SimplSharp.Library.clz";
-    public const string ManifestPath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Debug\net472\ProgramInfo.config";
+    public static readonly string ExePath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Tool", "bin", "Debug", "net8.0", "SimplSharp.Tool.exe");
+    public static readonly string TargetLibraryPath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Debug", "net472", "SimplSharp.Library.dll");
+    public static readonly string TargetLibraryProject = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "SimplSharp.Library.csproj");
+    public static readonly string TargetArchivePath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Debug", "net472", "SimplSharp.Library.clz");
+    public static readonly string ManifestPath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Debug", "net472", "ProgramInfo.config");
 #else
-    public const string ExePath = @"..\..\..\..\..\source\SimplSharp.Tool\bin\Release\net8.0\SimplSharp.Tool.exe";
-    public const string TargetLibraryPath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Release\net472\SimplSharp.Library.dll";
-    public const string TargetLibraryProject = @"..\..\..\..\..\source\SimplSharp.Library\SimplSharp.Library.csproj";
-    public const string TargetArchivePath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Release\net472\SimplSharp.Library.clz";
-    public const string ManifestPath = @"..\..\..\..\..\source\SimplSharp.Library\bin\Release\net472\ProgramInfo.config";
+    public static readonly string ExePath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Tool", "bin", "Release", "net8.0", "SimplSharp.Tool.exe");
+    public static readonly string TargetLibraryPath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Release", "net472", "SimplSharp.Library.dll");
+    public static readonly string TargetLibraryProject = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "SimplSharp.Library.csproj");
+    public static readonly string TargetArchivePath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Release", "net472", "SimplSharp.Library.clz");
+    public static readonly string ManifestPath = Path.Combine("..", "..", "..", "..", "..", "source", "SimplSharp.Library", "bin", "Release", "net472", "ProgramInfo.config");
 #endif
 
     /// <inheritdoc />
