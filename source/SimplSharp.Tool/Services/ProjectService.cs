53b3bfc886f1ce3e4740aebe40864cf67695a83b
Eric Williams <ewilliams0305@gmail.com>
2024-07-13 14:30:16 -0400

    created prokject service t query package references

--- source/SimplSharp.Tool/Services/ProjectService.cs
+++ source/SimplSharp.Tool/Services/ProjectService.cs
@@ -1,5 +1,91 @@
-﻿namespace SimplSharp.Tool;
+﻿using System.Xml;
+
+namespace SimplSharp.Tool;
 
 public sealed class ProjectService
 {
+
+    /// <summary>
+    /// Queries a csproj file for the Crestron Package references
+    /// </summary>
+    /// <param name="csprojFilePath">Full path for the CSProj</param>
+    /// <returns>The version of the SDK otherwise missing null</returns>
+    public Version? QueryProjectForSimplSharpLibraryVersion(string csprojFilePath)
+    {
+        if (!File.Exists(csprojFilePath))
+        {
+            return null;
+        }
+
+        var doc = new XmlDocument();
+        doc.Load(csprojFilePath);
+        var root = doc.DocumentElement;
+
+        if (root is null)
+        {
+            return null;
+        }
+
+        var itemGroups = root.GetElementsByTagName("ItemGroup");
+
+        foreach (XmlElement itemGroup in itemGroups)
+        {
+            var packageReferences = itemGroup.GetElementsByTagName("PackageReference");
+
+            foreach (XmlElement packageReference in packageReferences)
+            {
+                if (packageReference.GetAttribute("Include") != "Crestron.SimplSharp.SDK.Library")
+                {
+                    continue;
+                }
+
+                var version = packageReference.GetAttribute("Version");
+                return Version.Parse(version);
+            }
+        }
+
+        return null;
+    }
+
+    /// <summary>
+    /// Queries a csproj file for the Crestron Package references
+    /// </summary>
+    /// <param name="csprojFilePath">Full path for the CSProj</param>
+    /// <returns>The version of the SDK otherwise missing null</returns>
+    public Version? QueryProjectForSimplSharpProgramVersion(string csprojFilePath)
+    {
+        if (!File.Exists(csprojFilePath))
+        {
+            return null;
+        }
+
+        var doc = new XmlDocument();
+        doc.Load(csprojFilePath);
+        var root = doc.DocumentElement;
+
+        if (root is null)
+        {
+            return null;
+        }
+
+        var itemGroups = root.GetElementsByTagName("ItemGroup");
+
+        foreach (XmlElement itemGroup in itemGroups)
+        {
+            var packageReferences = itemGroup.GetElementsByTagName("PackageReference");
+
+            foreach (XmlElement packageReference in packageReferences)
+            {
+                if (packageReference.GetAttribute("Include") != "Crestron.SimplSharp.SDK.Program")
+                {
+                    continue;
+                }
+
+                var version = packageReference.GetAttribute("Version");
+                return Version.Parse(version);
+            }
+        }
+
+        return null;
+    }
 }
