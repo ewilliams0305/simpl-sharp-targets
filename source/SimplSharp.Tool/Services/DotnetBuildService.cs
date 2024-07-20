89f9fffc914730b4f6a50114c71deb48d59587c5
Eric Williams <ewilliams0305@gmail.com>
2024-07-20 15:19:22 -0400

    created plumbing for dotnet build

---
+++ source/SimplSharp.Tool/Services/DotnetBuildService.cs
@@ -0,0 +1,20 @@
+﻿namespace SimplSharp.Tool;
+
+public sealed class DotnetBuildService
+{
+    private readonly ProcessService _execute;
+
+    public DotnetBuildService(ProcessService execute)
+    {
+        _execute = execute;
+    }
+    public async Task BuildProject(string targetProjectOrDirectory)
+    {
+        var result = await _execute.Execute("dotnet", args: $"build {targetProjectOrDirectory}");
+
+        if (result.exitCode != 0)
+        {
+            throw new Exception($"Failed to compile project {targetProjectOrDirectory}");
+        }
+    }
+}
---
+++ source/SimplSharp.Tool/Services/ProcessService.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+
+namespace SimplSharp.Tool;
+
+internal sealed class ProcessService
+{
+
+    public async Task<(string output, int exitCode)> Execute(string executableName, string? args = null)
+    {
+        var processStartInfo = new ProcessStartInfo
+        {
+            FileName = executableName,
+            RedirectStandardOutput = true,
+            UseShellExecute = false,
+            CreateNoWindow = true,
+            Arguments = args,
+        };
+
+        using var process = new Process();
+        process.StartInfo = processStartInfo;
+        process.Start();
+        var output = await process.StandardOutput.ReadToEndAsync();
+        await process.WaitForExitAsync();
+
+        return (output, process.ExitCode);
+    }
+    
+}
